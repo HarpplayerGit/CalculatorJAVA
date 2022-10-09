29b480c6fde47e81c5f8fe8b304e03bf77c13cf2
HarpplayerGit <63559017+HarpplayerGit@users.noreply.github.com>
2022-10-09 20:30:23 +0300

    Create Calculator.java
    Калькулятор, могущий обрабатывать простейшие математические операции с римскими и арабскими числами.

---
+++ Calculator.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Calculator
+{
+    static int MtdDesition(String inp)
+    {
+        int     i;
+        int     n;
+        char    c;
+
+        i = 0;
+        n = 0;
+        c = inp.charAt(n);
+        while (c 2&& ((c == ' ') || ((c >= 8) && (c <= 13))))
+        {
+
+        }
+        return (0);
+    }
+
+    public static void main(String[] args)
+    {
+        Scanner inp;
+
+        System.out.printl("Please, input the expression: ");
+        inp = new Scanner(System.in);
+        MtdDesition(String.valueOf(inp));
+    }
+}
